02abe39204a8585c969a878370a609e99f43b228
Roel Napoles <leindaguen@gmail.com>
2026-08-10 19:38:11 -0400

    search by name

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 async function getData() {
-  const response = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=87422d08",
+  const searchInput = document.querySelector(".search__input");
+  const searchValue = searchInput.value;
+  const response = await fetch(
+    `http://www.omdbapi.com/?s=${searchValue}&apikey=87422d08`,
   );
   const data = await response.json();
-  console.log(data);
+
+  const results = document.querySelector(".results");
+
+  results.innerHTML = "";
+
+  data.Search.forEach((movie) => {
+    results.innerHTML += `<div class="movie__card">
+          <img src="${movie.Poster}" class="movie__poster" />
+          <div class="movie__info">
+            <h3 class="movie__title">${movie.Title}</h3>
+            <h2 class="movie__year">${movie.Year}</h2>
+          </div>`;
+  });
 }
